2173ac64e66d653f771d63f4fba4ca8357e989ea
botflux <victor.mendele68@gmail.com>
2019-01-05 23:00:08 +0100

    feat: page loading

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@
         <router-view name="footer" />
         </transition>
     </footer>
+    <vue-progress-bar></vue-progress-bar>
   </div>
 </template>
 
@@ -38,12 +39,30 @@ export default {
     AppHeader,
     AppNavigation
   },
-  apollo: {
-    
+  mounted () {
+    this.$Progress.finish()
   },
-  data () {
-    return {
-    }
+  created () {
+    //  [App.vue specific] When App.vue is first loaded start the progress bar
+    this.$Progress.start()
+    //  hook the progress bar to start before we move router-view
+    this.$router.beforeEach((to, from, next) => {
+      //  does the page we want to go to have a meta.progress object
+      if (to.meta.progress !== undefined) {
+        let meta = to.meta.progress
+        // parse meta tags
+        this.$Progress.parseMeta(meta)
+      }
+      //  start the progress bar
+      this.$Progress.start()
+      //  continue to next page
+      next()
+    })
+    //  hook the progress bar to finish after we've finished moving router-view
+    this.$router.afterEach((to, from) => {
+      //  finish the progress bar
+      this.$Progress.finish()
+    })
   },
   methods: {
     beforeEnter (e) { e.classList.add('fixed') },
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './vue-progressbar'
 import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
