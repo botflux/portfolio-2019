19cccd9d9abe7dc0d86b912ab519cb2c51656507
botflux <victor.mendele68@gmail.com>
2019-01-05 20:01:33 +0100

    feat: animation on project element

--- src/components/ProjectElement.vue
+++ src/components/ProjectElement.vue
@@ -1,5 +1,5 @@
 <template>
-    <article class="project__element grid grid--gapped">
+    <article class="project__element grid grid--gapped" ref="project">
         <img :src="filename" :alt="title" class="project__element__img mb-2 grid__column--4 grid__column--8--sm grid__column--4--md grid__column--6--lg">
         <div class="project__element__body grid__column--4 grid__column--8--sm grid__column--4--md grid__column--6--lg">
             <h2 class="heading heading--3 mb-2">{{ title }}</h2>
@@ -9,7 +9,20 @@
 </template>
 
 <script>
+import { TimelineMax } from 'gsap'
+
 export default {
+    mounted () {
+        const { project } = this.$refs
+        this.timeline = new TimelineMax({paused: true})
+        this.timeline.fromTo (project, .3, { opacity: 0 }, { opacity: 1})
+        this.timeline.play()
+    },
+    data () {
+        return {
+            timeline: {}
+        }
+    },
     props: {
         id: String,
         filename: String,
